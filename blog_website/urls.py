bb36596a1016c15f770cc29a2ea5f743696b861e
Nabil <44189517+MoinulHossainNabil@users.noreply.github.com>
2022-01-08 17:41:57 +0600

    Update urls.py

--- blog_website/urls.py
+++ blog_website/urls.py
@@ -25,6 +25,5 @@ urlpatterns = [
     path('', include('user_profile.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
